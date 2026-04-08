8f48a1b7b73d8ec337ce8b342470abcb69426a59
Anand <anandfairuzza@gmail.com>
2026-04-08 19:49:13 +0700

    Memperbaiki beberapa kesalahan di Buffer

--- buffer.h
+++ buffer.h
@@ -1,54 +1,43 @@
 #ifndef BUFFER_H
 #define BUFFER_H
-#define MAX_ROW      500
-#define MAX_COL      200
-#define HISTORY_SIZE  20
 
-
+#define MAX_ROW       500
+#define MAX_COL       200
+#define HISTORY_SIZE   20
 
 typedef struct {
     char text[MAX_ROW][MAX_COL];
     int  lineLength[MAX_ROW];
     int  totalLines;
-    int  currentRow;  
+    int  currentRow;
 } TextBuffer;
 
 typedef struct {
     TextBuffer buf;
-    
 } Snapshot;
 
 typedef struct {
     Snapshot entries[HISTORY_SIZE];
-    int      top; 
+    int      top;
 } Stack;
 
-
+/* Buffer */
 void buffer_init(TextBuffer *buf);
-
 void buffer_insert(TextBuffer *buf, const char *teks);
-
 void buffer_insert_baris(TextBuffer *buf, const char *teks);
-
 void buffer_backspace(TextBuffer *buf, int n);
-
 void buffer_hapus_baris(TextBuffer *buf);
-
 void buffer_goto(TextBuffer *buf, int nomor);
+void buffer_display(const TextBuffer *buf);
 
+/* Stack */
 void stack_init(Stack *s);
-
 void stack_push(Stack *s, const TextBuffer *buf);
-
 int  stack_pop(Stack *s, TextBuffer *buf);
 
-
-void buffer_push_undo(Stack *undo, Stack *redo_reset, const TextBuffer *buf);
+/* Undo / Redo */
+void buffer_push_undo(Stack *undo, Stack *redo, const TextBuffer *buf);
 int  buffer_undo(Stack *undo, Stack *redo, TextBuffer *buf);
 int  buffer_redo(Stack *undo, Stack *redo, TextBuffer *buf);
 
-<<<<<<< HEAD
-#endif 
-=======
-#endif 
->>>>>>> cfc70fc60063e08479e1129a245c30cecbb4166b
+#endif
